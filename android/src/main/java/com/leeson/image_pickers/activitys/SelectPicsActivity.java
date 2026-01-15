5189f8d364b229dac77f0c01e0ed3b97c745fb41
do <Jin_Do@126.com>
2026-01-15 20:55:00 +0800

    Update SelectPicsActivity.java

--- android/src/main/java/com/leeson/image_pickers/activitys/SelectPicsActivity.java
+++ android/src/main/java/com/leeson/image_pickers/activitys/SelectPicsActivity.java
@@ -348,13 +348,8 @@ public class SelectPicsActivity extends BaseActivity {
 
                             // 2. 针对“视频时长超过最大值”的提示
                             if (limitType == 8) {
-                                long durationSec = media.getDuration() / 1000 / 60;
-                                
-
-                                // 这里用你自己的最大秒数判断（你说的是 videoRecordMaxSecond）
-                                // 请选择5分钟以内的视频哦～
-                                String msg = "请选择"
-                                            + durationSec.intValue() + "分钟以内的视频哦~";
+                                int maxMinutes = videoRecordMaxSecond.intValue() / 60;
+                                String msg = "请选择" + maxMinutes + "分钟以内的视频哦~";
                                 Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
                                 // 返回 true：表示我们已经自定义提示，PictureSelector 不再弹默认提示
                                 return true;
